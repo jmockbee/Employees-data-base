0fcc5005a3b73b0eb969b7d487085c49ca0798e7
Jon Mockbee <mockbee1000@gmail.com>
2022-01-17 19:55:31 -0500

    Create and.sql
    new using and

---
+++ and.sql
@@ -0,0 +1,14 @@
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name = 'Denis' AND gender = 'M';
+    
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name = 'Kellie' AND gender = 'F';
+    
