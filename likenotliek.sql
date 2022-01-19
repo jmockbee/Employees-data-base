65485c8e5a64d0946ed11f071ded4c1c32816c77
Jon Mockbee <mockbee1000@gmail.com>
2022-01-18 19:46:10 -0500

    update

---
+++ likenotliek.sql
@@ -0,0 +1,54 @@
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name LIKE ('Mar_');
+    
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name LIKE ('Mark%');
+    
+SELECT 
+    *
+FROM
+    employees
+WHERE
+	hire_date like ('%2000%');
+    
+    
+SELECT 
+    *
+FROM
+    employees
+WHERE
+	hire_date like ('%2000%');
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+	emp_no like ('1000_');
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+	first_name like ('Jack');
+    
+    
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name NOT LIKE ('Jack');
+    
+
+    
+    
