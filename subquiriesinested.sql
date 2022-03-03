cae4bba7287551b2568ffdd4ffb5fa4f9739f5b3
Jon Mockbee <mockbee1000@gmail.com>
2022-03-02 19:23:52 -0500

    update subqueries

--- subquiriesinested.sql
+++ subquiriesinested.sql
@@ -12,3 +12,43 @@ WHERE
             dm.emp_no
         FROM
             dept_manager dm);
+            
+SELECT 
+    *
+FROM
+    dept_manager
+WHERE
+    emp_no IN (SELECT 
+            emp_no
+        FROM
+            employees
+        WHERE
+            hire_date BETWEEN '1990-01-01' AND '1995-01-01');
+            
+SELECT 
+    e.first_name, e.last_name
+FROM
+    employees e
+WHERE
+    EXISTS( SELECT 
+            *
+        FROM
+            dept_manager dm
+        WHERE
+            dm.emp_no = e.emp_no)
+ORDER BY emp_no;
+
+SELECT 
+    *
+FROM
+    employees e
+WHERE
+    EXISTS( SELECT 
+            *
+        FROM
+            titles t
+        WHERE
+            t.emp_no = e.emp_no
+                AND title = 'Assistant Engineer');
+            
+            
