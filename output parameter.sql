1283823096e19f757ebdfbbad8dcf52fa982b585
Jon Mockbee <mockbee1000@gmail.com>
2022-03-08 20:51:21 -0500

    variables update

--- output parameter.sql
+++ output parameter.sql
@@ -3,11 +3,11 @@ drop procedure if exists emp_salary;
 
 delimiter $$
 use employees $$
-create procedure  emp_avg_salary(in  p_emp_no  integer)
+create procedure  emp_avg_salary_out (in  p_emp_no  integer, out p_avg_salary Decimal (10,2) )
 begin 
 select
-	e.first_name, e.last_name, avg(s.salary)
-from
+	avg(s.salary)
+into p_avg_salary from 
 employees e 
 join
  salaries s on e.emp_no  = s.emp_no 
@@ -16,4 +16,3 @@ where e.emp_no  = p_emp_no;
 end $$
 delimiter ;
 
-call emp_avg_salary(11300)
